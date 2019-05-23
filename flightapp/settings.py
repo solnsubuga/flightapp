4628837bb2a9fda10e29fa43f6a5c54e3b996f7c
Solomon Nsubuga <snsubuga@live.com>
2019-05-23 18:06:50 +0300

    chore:Add requirements and SECRET_KEY to env variable

--- flightapp/settings.py
+++ flightapp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a@4snwx%y5dmx1@5nuyok6iocf)5-99n-9_cy%&2en4idcgv*r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
