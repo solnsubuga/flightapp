15375731d870f2fd88b24b5f29298b76e826ce79
Solomon Nsubuga <snsubuga@live.com>
2019-06-18 20:08:25 +0300

    Move static files

--- flightapp/settings.py
+++ flightapp/settings.py
@@ -139,13 +139,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 TOKEN_EXPIRATION_TIME = 360000
 TOKEN_ISSUER = 'https://demo-flightapp.io'  # This is fictional token issuer
