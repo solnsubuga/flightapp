72fc6ee55f359078994dd3646d4b20c3a279b417
Solomon Nsubuga <snsubuga@live.com>
2019-06-26 01:58:54 +0300

    [Feature #166135699]:Add endpoint to check flight status

--- flights/views.py
+++ flights/views.py
@@ -2,6 +2,7 @@
 from flights.serializers import FlightSerializer, FlightReservationSerializer, QueryReservationSerializer
 from flights.models import Flight, Reservation
 from rest_framework import generics
+from rest_framework import exceptions
 from rest_framework.views import APIView, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -61,3 +62,26 @@ class QueryReservationAPIView(APIView):
         return Response({
             'reservations': reservations_count
         })
+
+
+class CheckFlightStatusAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    @swagger_auto_schema(
+        responses={200: 'Ok', 404: 'Flight not found'}
+    )
+    def get(self, request, flight_number):
+        flight = Flight.objects.filter(number=flight_number).first()
+        if not flight:
+            raise exceptions.NotFound(
+                'Flight with number {flight_number} is not found'.format(flight_number=flight_number))
+        return Response({
+            'status': flight.status,
+            'info': {
+                'flight_number': flight.number,
+                'origin': flight.origin,
+                'destination': flight.destination,
+                'departure_time': flight.departure_time,
+                'arrival_time': flight.arrival_time
+            }
+        }, status=status.HTTP_200_OK)
